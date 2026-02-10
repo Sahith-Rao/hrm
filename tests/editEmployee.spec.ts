313930206ba6947680ffbd72dc63c6a219b4916e
Lingampally Sahith Rao <168744840+Sahith-Rao@users.noreply.github.com>
2026-02-10 11:31:32 +0530

    Modify employee test for updated first name format
    Updated employee creation and editing test to use new first name format.

--- tests/editEmployee.spec.ts
+++ tests/editEmployee.spec.ts
@@ -2,7 +2,6 @@ import { BaseTest as test } from "../src/basetest";
 
 test.describe("Edit Employee", () => {
   let createdEmployeeId: string;
-  let createdFirstName: string;
 
   test.beforeEach("Login and create employee for editing", async ({ loginPage, dashboardPage, viewEmployee, addEmployee }) => {
     await loginPage.navigate();
@@ -13,23 +12,32 @@ test.describe("Edit Employee", () => {
 
     await dashboardPage.clickPIM();
     await viewEmployee.clickAdd();
-    
-    const firstName = "sampleEdit";
+
+    const firstName = "chsampleEdit";
     const lastName = "sampleEdi";
-    
+
     await addEmployee.enterFirstName(firstName);
     await addEmployee.enterLastName(lastName);
-    await addEmployee.clickSave();
-    await addEmployee.verifyEmployeeAdded();
-    
+
     createdEmployeeId = await addEmployee.getEmployeeId();
     console.log(`Created employee with ID: ${createdEmployeeId} for editing`);
+
+    await addEmployee.clickSave();
+    await addEmployee.verifyEmployeeAdded();
+
+    await dashboardPage.logout();
+    await dashboardPage.verifyLogout();
   });
 
-  test("should edit employee first name", async ({ dashboardPage, employeeListPage, editPage }) => {
+  test("should edit employee first name", async ({ loginPage, dashboardPage, employeeListPage, editPage }) => {
+    await loginPage.enterUserName("Admin");
+    await loginPage.enterPassword("admin123");
+    await loginPage.clickLogin();
+    await loginPage.verifyLogin();
+
     await dashboardPage.clickPIM();
     
-    const updatedFirstName = "UpdatedSample";
+    const updatedFirstName = "chUpdatedSample";
     
     await employeeListPage.searchByEmployeeId(createdEmployeeId);
     await employeeListPage.verifyEmployeePresent(createdEmployeeId);
